ee78bfe707db757a0cbb952be34421415f04a638
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-06-02 14:53:37 -0300

    Rules for e-mail updated

--- scripts/script.js
+++ scripts/script.js
@@ -37,29 +37,48 @@ function checkData(event) {
     ]    
 
     for (let i in inputsValues) {
+
         if(inputsValues[i] === '') {
+            let voidMessage = voids[i]
             inputs[i].classList.add('js-block')
             voids[i].classList.add('js-block-message')
+            voidMessage.innerHTML = 'This field must be filled'
             event.preventDefault()
         } else {
             inputs[i].classList.remove('js-block')
             voids[i].classList.remove('js-block-message')
         }
     }
+
     if(checkName(fName, lName) === false) {
         event.preventDefault()
     }
-    checkEmail(inputsValues[2])
-    checkPassword(inputsValues[3])
+    checkEmail(email)
+    checkPassword(password)
 }
 
+function checkPassword(password) {
+    const shortPassword = voids[3]
+    if(password.length < 6 && password.length >= 1) {
+        inputs[3].classList.add('js-block')
+        voids[3].classList.add('js-block-message')
+        shortPassword.innerHTML = 'The password must contain at least 6 digits'
+        return false
+    } return true
+}
 
 function checkName(fName, lName) {
+    const fShortName = voids[0]
+    const lShortName = voids[1]
     if(fName.length === 1) {
         inputs[0].classList.add('js-block')
+        voids[0].classList.add('js-block-message')
+        fShortName.innerHTML = 'The Name is too short'
         return false
     } else if (lName.length === 1) {
         inputs[1].classList.add('js-block')
+        voids[1].classList.add('js-block-message')
+        lShortName.innerHTML = 'The Name is too short'
         return false
     } else {
         return true
@@ -67,25 +86,28 @@ function checkName(fName, lName) {
 }
 
 
-function checkPassword(password) {
-    if(password.length < 6) {
-        inputs[3].classList.add('js-block')
-        return false
-    } return true
-}
 
 
 function checkEmail(email) {
+    let emailMessage = voids[2]
+    let strEmail = email.split('')
+    let reverseEmail = strEmail.reverse().join('')
     if (email.includes('@')) {
         if (email.endsWith('.com') || email.endsWith('.com.br')) {
             inputs[2].classList.remove('js-block')
             voids[2].classList.remove('js-block-message')
+            if(reverseEmail.indexOf('@') === 7 || reverseEmail.indexOf('@') === 4) {
+                inputs[2].classList.add('js-block')
+                emailMessage.innerHTML = 'Looks like this is not an e-mail'
+            }
         } else {
             inputs[2].classList.add('js-block')
             voids[2].classList.add('js-block-message')
+            emailMessage.innerHTML = 'Looks like this is not an e-mail'
         }
     } else {
         inputs[2].classList.add('js-block')
         voids[2].classList.add('js-block-message')
+        emailMessage.innerHTML = 'Looks like this is not an e-mail'
     }
 }
